cab199158e882cd62325547ceb28acfb246ef974
ImInSpace <ignigomoreno@hotmail.com>
2016-06-17 23:33:16 +0200

    Loops K and N

--- supersopa/main.cpp
+++ supersopa/main.cpp
@@ -62,10 +62,10 @@ vector<int> english = {0, //distribución de las palabras segun su tamaño en in
                        0,
                        0};
 
-void generate_dictionary ()
+void generate_dictionary (int K)
 {
-    string file;
-    int K, seed;
+    /*string file;
+    int seed;
     cout<<"What file do you want to write to? (without extension) ";
     cin>>file;
     cout<<"How many words do you want? (K) ";
@@ -73,41 +73,32 @@ void generate_dictionary ()
     cout<<"What seed do you want? (-1 to time) ";
     cin>>seed;
     if (seed==-1)srand(time(NULL));
-    else srand(seed);
+    else srand(seed);*/
 
-    ofstream ofs (file+".dict");
-    ofs<<K<<endl;
+}
+
+vector<string> get_dictionary(int K)
+{
+    vector<string> dictionary (K);
     int printed=0, total=118615;//precalculado
     for (unsigned int i=0; i<english.size(); i++){
         for(int j=0; j<english[i]*K/total; j++){
+            string aux = "";
             for (unsigned int k=0; k<i; k++){
-                ofs<<rand()%10;
+                aux.push_back('0'+rand()%10);
             }
-            ofs<<endl;
+            dictionary[printed]=aux;
             printed++;
         }
     }
     while (printed<K){
+        string aux = "";
         for (unsigned int k=0; k<8; k++){
-            ofs<<rand()%10;
+            aux.push_back('0'+rand()%10);
         }
-        ofs<<endl;
+        dictionary[printed]=aux;
         printed++;
     }
-    ofs.close();
-}
-
-vector<string> get_dictionary()
-{
-    cout << "What dictionary file do you want to open? (without extension) ";
-    string file;
-    cin>>file;
-    ifstream ifs (file+".dict");
-    int K;
-    ifs>>K;
-    vector<string> dictionary (K);
-    string aux;
-    for (unsigned int i=0; i<dictionary.size(); i++) ifs>>dictionary[i];
     return dictionary;
 }
 
@@ -159,39 +150,32 @@ void print_board(vector<vector<char>> board){
 
 int main()
 {
-    cout << "What do you want to do?" << endl;
-    cout << "1-generate a dictionary file" << endl;
-    cout << "2-call solver" <<endl;
-    int initialOption;
-    cin>>initialOption;
-    if (initialOption==1){
-        generate_dictionary();
-    }
-    else{
         //bool calculate_time=ask_question("Do you want to calculate time?");
         Solver* solver=get_solver();
-
-        vector<string> dictionary=get_dictionary();
-        solver->initDictionary(dictionary);
-
+        vector<string> dictionary;
         //vector<vector<char>> board=get_board();
         //solver->initBoard(board);
         //print_board(board);
         
         clock_t begin_time;
+        srand(0);
         vector<vector<char>> board;
         for (int N=3000; N<13000; N+=2000){
-                srand(0);
+
                 board = vector<vector<char>> (N, vector<char> (N));
                 for (int i=0; i<N; i++){
                     for (int j=0; j<N; j++)board[i][j]='0'+(rand()%10);
                 }
-                solver->initDictionary(dictionary);
-                solver->initBoard(board);
-                begin_time = clock();
-                solver->solve();
-                cout<< float( clock () - begin_time ) /  CLOCKS_PER_SEC <<endl;
-                
+
+                for (int K=3000; K<13000; K+=2000){
+                    dictionary=get_dictionary(K);
+                    solver->initDictionary(dictionary);
+                    solver->initBoard(board);
+                    begin_time = clock();
+                    solver->solve();
+                    cout<< float( clock () - begin_time ) /  CLOCKS_PER_SEC <<" ";
+                }
+                cout<<endl;
         }
         /*
         if (calculate_time)begin_time = clock();
@@ -208,12 +192,16 @@ int main()
             if (calculate_time)cout << "Time spent solving: "<< float( clock () - begin_time ) /  CLOCKS_PER_SEC;
             solver->printSolution();
         }*/
-    }
+
     return 0;
 }
 
 
-
+2.956 3.587 4.441 5.408 5.726
+5.563 6.227 8.184 8.351 9.76
+9.654 10.438 11.702 12.56 12.811
+14.415 15.672 16.118 17.62 18.444
+20.517 21.269 22.527 23.086 24.226
 
 
 
